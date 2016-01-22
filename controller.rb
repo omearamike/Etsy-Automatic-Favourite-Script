41f88128c5ce297a4be7def545aca327076bb4ba
omearamike <mikeytoomo@hotmail.com>
2016-01-22 17:21:11 +0000

    Issue: Sendmessage not sending all messages
    Issue: Only one message sending each time not all the messages that
    need to be sent

--- controller.rb
+++ controller.rb
@@ -1,13 +1,11 @@
 require_relative 'main'
+require 'benchmark'
 require 'json'
 
-initilize = Initialize.new
-
-getsettings = Getsettings.new
-
-getsettings._getApikey
-initilize.prepareEnv
+# Creates the settings and folders for the app to run
 def createSettings
+	initilize = SetupApplication.new
+	initilize.prepareEnv
 	if File.exist?('settings.json') == false
 		print "Would you like to create settings for this app? Y/n? "
 		settings = gets.chomp
@@ -17,94 +15,112 @@ def createSettings
 			print "\nYou will need to create settings to use this app! \n\n"
 		end
 	else 
-		print "Super, All settings are already set.\n(Edit your settings.json file if needed.)\n"
+		# print "Super, All settings are already set.\n(Edit your settings.json file if needed.)\n"
 	end
 end #End of createSettings Method
-createSettings
-# Test the session Class
-	def sessionTest
-		mytestAccount = 80353922
-		session = Session.new
-		session.auth(@USERNAME, @PASSWORD)
-		session.submitComment(mytestAccount)
+
+# Settings saved in run time memory for use in the application
+def tempSettings
+	getsettings = SetupApplication.new
+	@APIKEY = getsettings._getApikey
+	@USERID = getsettings._getUserid
+	@USERNAME = getsettings._getUsername
+	@PASSWORD = getsettings._getPassword
+	@LISTINGARRAY = getsettings._getListingArray
+	@LISTINGCOUNT = getsettings._getTotalListings
+	@SUBJECT = getsettings._getSubject
+	@MESSAGE = getsettings._getMessage
+end
+
+# Print all the current settings
+def printSettings
+	puts "API Key = " + @APIKEY
+	puts "User ID = " + @USERID
+	puts "Username = " + @USERNAME
+	puts "Password = " + @PASSWORD	
+
+	z = 0 
+	x = @LISTINGCOUNT
+    while x != z do
+	puts "Listings #{z + 1} = " + @LISTINGARRAY[z]['listingid'].to_s
+	z = z + 1
 	end
-# sessionTest #Run the sessionTest Method
-# prepenv
 
-# Test the User Class
-	def testListing
-		listing = Listing.new
-		listing.getjson(127179505, @APIKEY)
+	puts "Subject = " + @SUBJECT
+	puts "Message = " + @MESSAGE
+end
+
+# Gets JSON data of all favourites for all listings
+def getJson
+	storeuser = Storeuser.new
+	z = 0 
+	x = @LISTINGCOUNT
+    while x != z do
+	storeuser.getjson(@LISTINGARRAY[z]['listingid'].to_s, @APIKEY)
+	z = z + 1
 	end
-# testListing #Run the testUser Method
+end
 
-	def testDatabase
-		database = Database.new
-		database.createDB
-		database.addUser
+# Creates and formats new database if not already there also returns database instance 
+def newDatabase
+	storeuser = Storeuser.new
+	storeuser._newDatabase
+end
+
+# Adds users based on listing id to the database
+def addUsers
+	storeuser = Storeuser.new
+	z = 0 
+	x = @LISTINGCOUNT
+    while x != z do
+	storeuser.addUsers(@LISTINGARRAY[z]['listingid'].to_s)
+	z = z + 1
 	end
+end
 
-# testDatabase
+# Signin with spider
+def getAuthenticated
+	websession = WebSession.new
+	auth = websession._getAuthenticated(@USERNAME, @PASSWORD)
+end
+
+def sendMessage
+	websession = WebSession.new
+	array = websession.getUserids
+	# agent = getAuthenticated
+	# puts array
+    array.each do |index|
+    	websessionn = WebSession.new
+
+      # ind = index.to_s
+      # ind = ind.delete('[').to_i
+      ind = 179
+      websessionn._sendMessage(ind, @SUBJECT, @MESSAGE) 
+    end
+end
 
 
-def run(listingId)
+def runApplication
+	createSettings # Sets environment and creates Settings file if it does not exist
+	tempSettings # Stores settings in Application runtime memory
+	getJson # Gets seperate JSON files of all favourites for each listing
+	newDatabase # Prepares database if has not been created yet
+	addUsers # Adds users based on listing id to the database
+	sendMessage
+	# puts websession.this
+end
+time = Benchmark.measure {
+	runApplication
+ }
+ puts time.real #or save it to logs
+def testApplication
+	printSettings # Print all current Settings
 
-	listing = Listing.new
-	@session = Session.new
-	database = Database.new
-
-	@mytestAccount = 80353922
-	
-	#Getting JSON Data based on the Listing ID
-	listing.getjson(listingId, @APIKEY)
-
-	#Creating Database if it doesnt exist 
-	database.createDB
-	#Also adding JSON Data to the Database
-	database.addUser
-
-	@db = SQLite3::Database.open "Database/FavList.db"
-
-		def upload #counts how many records in the database
-		    results = @db.get_first_row "select Id from FavList order by Id desc limit 1"
-		    results = results.join.to_i
-		    @max_id = results + 1
-		end #End of upload
-		upload
-
-	#Add user to the database
-	def addUser
-		#Creating Auth Session
-		@session.auth(@USERNAME, @PASSWORD)
-
-		@total_id = @max_id
-		@max_id.times do
-		    if @max_id >= 2
-		            @max_id = @max_id - 1
-		            @updated = @db.get_first_value "SELECT MessageSent FROM FavList WHERE Id='#{@max_id}'"
-		            puts @updated 
-		            puts @max_id
-
-		        if @updated == "FALSE"
-		            @user_id = @db.get_first_value "SELECT UserId FROM FavList WHERE Id='#{@max_id}'"
-		            # puts @user_id
-		            @db.execute "UPDATE FavList SET MessageSent='TRUE' WHERE Id='#{@max_id}'"
-		            @session.submitComment(@user_id)
-					# puts @user_id
-		        else 
-
-		        end
-		    else 
-		    	puts "Everything upto date"
-		    end
-
-		end
-				#Submitting Comment to everyone in the Database
-	end #End of addUser
-	addUser
-end #End of run
+end
+# testApplication
 
 # run(187018850)
 # run(127179505)
 # run(208315501)
 # run(260860360)
+
--- main.rb
+++ main.rb
@@ -7,214 +7,282 @@ require 'mechanize'
 require 'logger'
 require 'json'
 require 'sqlite3'
+require 'logger'
 
-# Sets up Appliction to run for the first time
-class Initialize
+
+$log = Logger.new('Log/weekly.log', 'weekly')
+      # $log.error "This works yes"
+
+# Sets up Appliction to run for the first time.
+class SetupApplication
 # Creates the neccessary Directorys for the application to run
 	def prepareEnv
 		Dir.mkdir("WorkingFiles") unless File.exists?("WorkingFiles")
 		Dir.mkdir("Log") unless File.exists?("Log")
 		Dir.mkdir("Database") unless File.exists?("Database")
-	end 
-
+  end
 # Allows listings Ids to be dynamically added to the settings file
-	YourStruct = Struct.new(:id, :listing)
-	class YourStruct
-		  def to_json(*a)
-		    {:id => self.id, :listing => self.listing}.to_json(*a)
-		  end
+ 	YourStruct = Struct.new(:id, :listingid)
+ 	class YourStruct
+ 		  def to_json(*a)
+ 		    {:id => self.id, :listingid => self.listingid}.to_json(*a)
+ 		  end
+ 
+ 		  def self.json_create(o)
+ 		    new(o['id'], o['listingid'])
+ 		  end
+ 	end
 
-		  def self.json_create(o)
-		    new(o['id'], o['car'])
-		  end
-	end
-
-# Takes the revelent Application Settings from User
+# Creates the Settings file to run the application
 	def createSettings
+    $log.info "Creating Application settings"
 		print "Enter your API Key: "
 		apikey = gets.chomp
+    print "Enter your User Id: "
+    userid = gets.chomp
 		print "Enter your Username: "
 		username = gets.chomp
 		print "Enter your Password: "
 		password = gets.chomp
 
-		listing = "x", x = 1, b = [ YourStruct.new("Nil", "Nil")] # Varibles needed for the next bit of code
-		while listing != "done" do
-		print "Enter the Listing ID Numbers you would like to add (When your finished type done): "
-		listing = gets.chomp
-		a = [ YourStruct.new(x, listing)]
-		allListings = b + a
-		x = x + 1
-		end
-		allListings.pop
-		allListings.shift
+		listing = "x", x = 1, allListings = [ YourStruct.new("Nil", "Nil")] # Varibles needed for the next bit of code
+ 		while listing != "done" do
+ 		print "Enter the Listing ID Numbers you would like to add (When your finished type done): "
+ 		listing = gets.chomp
+ 		a = [ YourStruct.new(x, listing)]
+ 		allListings = allListings + a
+ 		x = x + 1
+ 		end
+ 		allListings.pop
+ 		allListings.shift
 
-	  	print "Enter your Subject Line: "
-	  	subject = gets.chomp
-	  	print "Enter your Desired Message: "
-	  	message = gets.chomp
-	  	print "Good Job, Settings are Ready.\n (Edit your settings.json file if needed.)\n"
+ 	  	print "Enter your Subject Line: "
+ 	  	subject = gets.chomp
+ 	  	print "Enter your Desired Message: "
+ 	  	message = gets.chomp
+ 	  	print "Good Job, Settings are Ready.\n (Edit your settings.json file if needed.)\n"
+ 
+ 	  	tempHash = {
+ 	  	"LoginCredentials" => {
+ 	  		"ApiKey" => apikey,
+          "UserId" => userid,
+    			"Username" => username,
+    			"Password" => password
+ 	  	},
+ 	  	"Listings" => allListings,
+ 	  	"Message" => {
+ 	  		"Subject" => subject,
+ 	  		"Message" => message
+ 	  	}	
+ 	}
 
-	  	tempHash = {
-	  	"LoginCredentials" => {
-	  		"ApiKey" => apikey,
-   			"Username" => username,
-   			"Password" => password
-	  	},
-	  	"Listings" => allListings,
-	  	"Message" => {
-	  		"Subject" => subject,
-	  		"Message" => message
-	  	}	
-	}
 		tempHash = tempHash.to_json
-        tempHash = JSON.parse(tempHash)
-    	tempHash = JSON.pretty_generate(tempHash)
+    tempHash = JSON.parse(tempHash)
+    tempHash = JSON.pretty_generate(tempHash)
 		File.open("settings.json","w") do |f|
 		f.write(tempHash)
 		end
 	end # createSettings
-end #End of Initialize Class
 
+  def jsonOpen
+    file = open("settings.json")
+    file = file.read
+    file = JSON.parse(file)
+  end
 
-# Class to retrieve the settings data from Json file
-class Getsettings
+     def _getApikey
+      apiKey = jsonOpen['LoginCredentials']['ApiKey']
+      return apiKey
+     end
 
-	def jsonOpen
-		file = open("settings.json")
-		file = file.read
-		@file = JSON.parse(file)
-	end
-
-	def _getApikey
-		jsonOpen
-  		@ApiKey = @file['LoginCredentials']['Password']
-  		return @ApiKey
+     def _getUserid
+      userid = jsonOpen['LoginCredentials']['UserId']
+      return userid
      end
 
      def _getUsername
-		jsonOpen
-  		@username = @file['LoginCredentials']['Username']
-  		return @username
+      username = jsonOpen['LoginCredentials']['Username']
+      return username
      end
 
      def _getPassword
-		jsonOpen
-  		@password = @file['LoginCredentials']['Password']
-  		return @password
+      password = jsonOpen['LoginCredentials']['Password']
+      return password
      end
 
      def _getSubject
-		jsonOpen
-  		@subject = @file['Message']['Subject']
-  		return @subject
+      subject = jsonOpen['Message']['Subject']
+      return subject
      end
-end # End of GetSettings
 
-class Session
-	# Takes the username and password creates
-	def auth(username, password)
-		agent = Mechanize.new
-		agent.get("https://www.etsy.com/ie/signin")
-		agent.log = Logger.new "Log/mechanize.log"
-		agent.page.forms[2]
-		agent.page.forms[2]["username"] = username
-		agent.page.forms[2]["password"] = password
-		loginin = agent.page.forms[2].submit
-		@agent = agent
-	end
+     def _getMessage
+      message = jsonOpen['Message']['Message']
+      return message
+     end
 
-	# Puts userid into link and takes the subject and the message
-	def submitComment(userid, subject = "Subject", message = "Message")
-                agent = @agent
-                page = agent.page.body
-                favourites = agent.get("https://www.etsy.com/ie/conversations/new?with_id=#{userid}&ref=pr_contact")
-                body = favourites.body
-                page = Nokogiri::HTML(body)
-                name = page.css('span.default-recipient')[0].text
-                name = name.split(" ")[0]
-                @name = name.capitalize
-                agent.page.forms[2]["subject"] =  "Hey #{@name}, thanks for the ❤️"
-                agent.page.forms[2]["message"] = "Hey #{@name},\n
-        Thank you for giving our shop some ❤️. If anything in particular catches your eye you can use this coupon code SHARETHELOVE to get 20% Off any item on our store.\n
-        http://bit.ly/favTMC \n
-        Hope to see you soon, \n
-        Mike | The Moose Creative |"
-                send = agent.page.forms[2].submit
-        end
+     def _getTotalListings
+      count = jsonOpen['Listings'].count
+      return count
+     end
 
+     def _getListing(index)
+      listingid = jsonOpen['Listings'][index]['listingid']
+      return listingid
+     end
 
+     def _getListingArray
+      listingArray = jsonOpen['Listings']
+      return listingArray
+     end
+end #End of SetupApplication Class
 
-end
+class Storeuser
 
+  # Takes the listing ID and apikey as an argument creates JSON file for each listing
+  def getjson(listingid, apikey)
+      $log.info "New Json File Listing: #{listingid}" unless File.exists?("WorkingFiles/data_#{listingid}.json")
+      response = open("https://openapi.etsy.com/v2/listings/#{listingid}/favored-by?api_key=#{apikey}").read
+      response = JSON.parse(response)
+      response = JSON.pretty_generate(response)
+      auth_file = File.open("WorkingFiles/data_#{listingid}.json", "w")
+      auth_file.print response
+      auth_file.close 
+  end
 
+# Creates a New Database if needed
+  def _newDatabase
+    db = SQLite3::Database.new "Database/Favourites_users.db"
+    db.execute "CREATE TABLE IF NOT EXISTS FavouriteUsers(Id INTEGER PRIMARY KEY, DATE TEXT, ListingId INT, UserId INT, MessageSent INT)"
+    return db
+  end
 
-class Listing
+# Add users from Json file to the database
+	def addUsers(listingid)
+    file = open("WorkingFiles/data_#{listingid}.json")
+    json = file.read
+    parsed = JSON.parse(json)
+    parsed['results'].each do |child|
+    # db = _newDatabase
+    @listing_id = child['listing_id']
+    @user_id = child['user_id']
+        if @user_id != nil
+          @user_id = @user_id
 
-	# Takes the listing ID as an argument
-	def getjson(listingid, apikey)
-        response = open("https://openapi.etsy.com/v2/listings/#{listingid}/favored-by?api_key=#{apikey}").read
-        response = JSON.parse(response)
-    	response = JSON.pretty_generate(response)
-    	auth_file = File.open("WorkingFiles/data.json", "w")
-    	auth_file.print response
-    	auth_file.close 
-	end
-end #End of Listing Class
-
-
-class Database
-
-	#Creates the Database if it does not exist
-	def createDB
-		@root = File.dirname(__FILE__)
-		begin
-		    # SQLite3::Database.new "SANDBOX_DB.db"
-		    db = SQLite3::Database.new "Database/FavList.db"
-		    db.execute "CREATE TABLE IF NOT EXISTS FavList(Id INTEGER PRIMARY KEY, DATE TEXT, ListingId INT, UserId INT, MessageSent INT)"
-
-		rescue SQLite3::Exception => e 
-		    
-		    puts "Exception occurred"
-		    puts e  
-		ensure
-		    db.close if db
-		end
-	end #End of createDB Method
-
-	#Add user to Database
-	def addUser
-		file = open("WorkingFiles/data.json")
-        json = file.read
-        parsed = JSON.parse(json)
-        parsed['results'].each do |child|
-        @listing_id = child['listing_id']
-        @user_id = child['user_id']
-        if @user_id == nil
-        	@user_id = 0000000
-        else
-        	@user_id = @user_id
-        end
-
-
-        db = SQLite3::Database.open "Database/FavList.db"
-        id = db.get_first_row "SELECT UserId FROM FavList WHERE UserId='#{@user_id}'" 
-
-        # Ensures no duplicates and then adds the record
+        db = _newDatabase
+        id = db.get_first_row "SELECT UserId FROM FavouriteUsers WHERE UserId='#{@user_id}'" 
         def existsCheck( db, id )
-            testing = db.execute( "select 1 from FavList where UserId = ?", [id] ).length > 0
-            
+            testing = db.execute( "select 1 from FavouriteUsers where UserId = ?", [id] ).length > 0
+
             if testing == true   
             else 
-            db.execute( "INSERT INTO FavList ( DATE, ListingId, UserId, MessageSent ) VALUES ( CURRENT_TIMESTAMP, '#{@listing_id}', '#{@user_id}', 'FALSE' )" )
+            db.execute( "INSERT INTO FavouriteUsers ( DATE, ListingId, UserId, MessageSent ) VALUES ( CURRENT_TIMESTAMP, '#{@listing_id}', '#{@user_id}', 'TRUE' )" )
             end
 
         end #End of existsCheck Method
             
         existsCheck(db, id)
 
-    	end #End of do loop
-	end #End of addUser Method
+        else
+          $log.warn "No UserId for user from Listing: #{listingid}" 
+        end
+      end #End of do loop
+  end
 
+  def totalRecords
+    _newDatabase
+    results = _newDatabase.get_first_row "select Id from FavouriteUsers order by Id desc limit 1"
+    results = results.join.to_i
+    @max_id = results + 1
+  end
 end #end of Database Class
 
+
+
+class WebSession
+
+  # Takes the username and password creates
+  def _getAuthenticated(username, password)
+
+    url = "https://www.etsy.com/ie/signin"
+
+  resp = Net::HTTP.get_response(URI.parse(url))
+    if resp.code.match(/20\d/)
+      $log.info "Authentication Success: Status #{resp.code}"
+    elsif resp.code.match(/40\d/)
+      $log.fatal "Authentication Failed: Status #{resp.code}"
+    else
+      $log.debug "Authentication Potential Issue: Status #{resp.code}"
+    end
+    
+    agent = Mechanize.new
+    #handle the sign in stuff
+    page = agent.get(url)
+    page = page.body
+    agent.log = Logger.new "Log/mechanize.log"
+    agent.page.forms[2]
+    agent.page.forms[2]["username"] = username
+    agent.page.forms[2]["password"] = password
+    loginin = agent.page.forms[2].submit
+    agent.cookie_jar.save_as("cookies.yml")
+    $log.info "Authenication Cookie: Successfully Obtained"
+  end
+
+  # Outputs list of the Index of all the records in databasee that are set to true
+  def getUserids
+    storeuser = Storeuser.new
+    db = storeuser._newDatabase
+    notUploadedIds = db.execute ("SELECT Id FROM FavouriteUsers WHERE MessageSent = 'TRUE'")
+    return notUploadedIds
+  end
+
+$agent = Mechanize.new
+
+  # Puts userid into link and takes the database index, subject and message as aurguemnts
+  def _sendMessage(index, subject, message)
+
+
+  websession = WebSession.new
+  auth = websession._getAuthenticated('urbanmooseapparel', 'Mikemeara1995')
+
+# Checks if the cookies are valid
+    
+    $agent.cookie_jar.load('cookies.yml')
+    cookies = $agent.cookies
+    cookies.each do |c|
+    $log.warn "Session Cookie: Expired #{c.name} " if "#{c.expired?}" == "true"
+    @flag = true if c.expired? == true 
+    end # End of do
+
+# If the cookie is not valid it will attempt to get new Session Cookie
+    if @flag == true
+        tempCred = SetupApplication.new
+        tempUsername = tempCred._getUsername
+        tempPassword = tempCred._getPassword
+        _getAuthenticated(tempUsername, tempPassword)
+      else
+    end
+
+    db = SQLite3::Database.new "Database/Favourites_users.db"
+    userId = db.execute "SELECT UserId FROM FavouriteUsers WHERE Id='#{index}'" # Selects Userid
+    userId = userId[0][0]
+    url = "https://www.etsy.com/conversations/new?with_id=#{userId}&ref=pr_contact" # Forms the link which will be used to send message
+    puts url
+    page = $agent.get(url)
+    body = page.body
+    page = Nokogiri::HTML(body)
+    name = page.css('span.default-recipient').text
+    name = name.split(" ")[0]
+    @name = name.capitalize 
+
+    # Swaps out name in string with the name obtained from the webpage
+    subject = subject.gsub! '#name', @name 
+    message = message.gsub! '#name', @name 
+
+    # Send the message out
+    $agent.page.forms[2]["subject"] =  subject
+    $agent.page.forms[2]["message"] = message
+    $agent.page.forms[2].submit
+    # db.execute "UPDATE FavouriteUsers SET MessageSent='FALSE' WHERE Id='#{index}'" #update Record to  True only if all the above was success full
+    $log.info "Message Sent: Database Index = #{index} UserID = #{userId} Name = #{@name}"
+  end
+end
